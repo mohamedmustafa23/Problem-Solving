74a8b63a33d470793c517767d5196d55ec25915c
MohamedMustafa <MohamedMustafa4321@gmail.com>
2026-04-14 13:44:03 +0200

    Add: LeetCode #25 - Remove Element solution

--- Problem_Solving/Problems/Arrays/RemoveElement.cs
+++ Problem_Solving/Problems/Arrays/RemoveElement.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Problem_Solving.Problems.Arrays
+﻿namespace Problem_Solving.Problems.Arrays
 {
     public class RemoveElement
     {
+        //Two Pointers
+        public int RemoveElementFunction(int[] nums, int val)
+        {
+            int k = 0; 
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)  
+                {
+                    nums[k] = nums[i]; 
+                    k++;               
+                }
+            }
+
+            return k; 
+        }
     }
 }
--- Problem_Solving/Program.cs
+++ Problem_Solving/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Problem_Solving
+﻿using Problem_Solving.Problems.Arrays;
+
+namespace Problem_Solving
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = [3, 2, 2, 5, 3];
+            RemoveElement removeElement = new RemoveElement();
+
+            int newarr = removeElement.RemoveElementFunction(arr, 3);
+
+            Console.WriteLine(newarr);
         }
     }
 }
